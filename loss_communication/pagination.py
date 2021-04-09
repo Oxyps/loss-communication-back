229924d484e0bc1ee784d126913b5e671b48fbc8
Abel Castro <abel_castro_junior@hotmail.com>
2021-04-09 00:49:28 -0300

    add pagination to response

---
+++ loss_communication/pagination.py
@@ -0,0 +1,25 @@
+from rest_framework import pagination
+from rest_framework.response import Response
+
+class Pagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_query_param = 'page'
+
+    def get_previous_page(self):
+        if not self.page.has_previous():
+            return None
+        return self.page.previous_page_number()
+
+    def get_next_page(self):
+        if not self.page.has_next():
+            return None
+        return self.page.next_page_number()
+
+    def get_paginated_response(self, data):
+        return Response({
+            'previous_page': self.get_previous_page(),
+            'next_page': self.get_next_page(),
+            'page_size': self.page_size,
+            'length': self.page.paginator.count,
+            'data': data
+        })
